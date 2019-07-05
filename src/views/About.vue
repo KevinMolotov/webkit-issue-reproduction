40e81dce0ea6a73d590ee056037d788c5822419c
Kévin Lambert <klambert@molotov.tv>
2019-07-05 12:33:40 +0200

    Add more items

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,21 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <HelloWorld v-for="i in 150" :key="i" msg="prout"/>
   </div>
 </template>
+<script>
+export default {
+  name: 'about',
+
+  components: {
+    HelloWorld: () => import('@/components/HelloWorld.vue'),
+  },
+
+  mounted() {
+    setTimeout(() => {
+      this.$router.push('/');
+    }, 500);
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld v-for="i in 200" :key="i" msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
@@ -13,6 +13,12 @@ export default {
   name: 'home',
   components: {
     HelloWorld,
+  },
+
+  mounted() {
+    setTimeout(() => {
+      this.$router.push('/about');
+    }, 500);
   },
 };
 </script>
